d63622c40b29a4a8cb032a2be6615be484d187c7
Yash Solanki <ysolanki2013@gmail.com>
2023-04-29 20:29:59 +0530

    Init app

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, reset } from "../features/auth/authSlice";
+
+const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate("/");
+  };
+
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">Support Desk</Link>
+      </div>
+      <ul>
+        {user ? (
+          <button className="btn" onClick={onLogout}>
+            <FaSignOutAlt /> Logout
+          </button>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">
+                <FaSignInAlt /> Login
+              </Link>
+            </li>
+            <li>
+              <Link to="/register">
+                <FaUser /> Register
+              </Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
